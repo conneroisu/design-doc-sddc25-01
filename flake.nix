92b46f7edb230de418fe1b93847e7b1ff066ed19
connerohnesorge <conneroisu@outlook.com>
2025-05-09 09:33:15 -0500

    latest

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Design Doc for SDDC 25-01";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    bun2nix.url = "github:baileyluTCD/bun2nix";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+  };
+
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} (
+      {self, ...}: {
+        systems = import inputs.systems;
+        imports = [
+          ./nix/flake-module.nix
+        ];
+
+        perSystem = {
+          self',
+          config,
+          inputs',
+          pkgs,
+          system,
+          ...
+        }: {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [
+              (final: prev: {
+                go = prev.go_1_24;
+              })
+            ];
+          };
+        };
+      }
+    );
+}
---
+++ nix/flake-module.nix
@@ -0,0 +1,18 @@
+{
+  perSystem = {
+    self',
+    config,
+    inputs',
+    pkgs,
+    system,
+    ...
+  }: {
+    devShells.default = pkgs.mkShell {
+      packages = with pkgs; [
+        texlive.combined.scheme-full
+        texlab
+        tectonic
+      ];
+    };
+  };
+}
