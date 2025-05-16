db6827e851aa4b96e4dda98eda6b43d1d5f74821
connerohnesorge <conneroisu@outlook.com>
2025-05-16 09:48:28 -0500

    latest

--- nix/flake-module.nix
+++ nix/flake-module.nix
@@ -7,18 +7,44 @@
     system,
     ...
   }: {
-    devShells.default = pkgs.mkShell {
+    devShells = let
       packages = with pkgs; [
-        nixd
-        statix
-        deadnix
+        nixd # Nix
         alejandra
-        texlive.combined.scheme-full
+
+        texlive.combined.scheme-full # LaTeX
         texlab
         tectonic
         texliveTeTeX
         pandoc
       ];
+      shellHook = ''
+        echo "Welcome to the Nix Shell"
+      '';
+    in {
+      default = pkgs.mkShell {
+        inherit packages shellHook;
+      };
+      devcontainer = pkgs.mkShell {
+        inherit shellHook;
+        packages =
+          packages
+          ++ (with pkgs; [
+            git
+            direnv
+            nix-direnv
+            gnumake
+            gnused
+            gnugrep
+            gnutar
+            gzip
+            curl
+            jq
+            git-lfs
+            coreutils-full
+          ]);
+      };
     };
+    # TODO: Render Package
   };
 }
